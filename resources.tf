46fa362a6cf205a2ae7a43a5d0b33b8e1e5aa8b7
Fabian Brash <fabianbrash@gmail.com>
2025-02-18 15:21:58 -0500

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["70.105.14.113/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
